17276ebe4e0b157868c7a176408360c12d0e5c87
Celal <binaycelalselim@gmail.com>
2024-10-27 00:32:08 +0300

    files added

---
+++ Front-End/Assignment/5 - JS Data Types/data_types1.js
@@ -0,0 +1,47 @@
+let dec = 7.67;
+console.log(Math.floor(dec));
+
+console.log(Math.random().toFixed(3));
+
+console.log(Math.min(5, 10, -2, 0, 22));
+
+console.log(Math.pow(2, 3));
+
+let text = "JavaScript";
+console.log(text.substring()); //! text metninin her harfine ulaşma
+console.log(text[4]);
+
+let text1 = "Celal";
+let text2 = "Selim";
+console.log(text1 + text2);
+
+let text3 = "Celal \nSelim \nBinay";
+console.log(text3);
+
+let isSmall = 4 < 10;
+console.log(isSmall);
+
+let a;
+console.log(a);
+
+function example() {
+  let sum = 65 + 32;
+}
+console.log(example());
+
+let schoolNumbers = [282, 85, 355, 435, 505, 792];
+console.log(schoolNumbers[4]);
+
+let obj = { name: "Hellen", age: 39 };
+console.log(obj.name);
+
+let val1 = 10;
+let val2 = "30";
+console.log(val1 + val2);
+
+let newVal = parseInt(val2);
+console.log(newVal);
+
+const now = new Date();
+const year = now.getFullYear();
+console.log(year);
---
+++ Front-End/Recap/js-data-types/javas.js
@@ -0,0 +1,36 @@
+// ! Bir sayının karesini hesaplayınız.
+
+// let num = prompt("Bir sayı giriniz:");
+// let square = Math.pow(num, 2);
+// console.log(square);
+
+// ! Kullanıcıdan alınan ondalık sayıyı aşağı ve yukarı yuvarlayınız.
+
+// let num1 = prompt("Ondalıklı bir sayı giriniz");
+// let newNum1 = Math.ceil(num1);
+// let newNum2 = Math.floor(num1);
+// console.log(newNum1);
+// console.log(newNum2);
+
+// ! Bir dizideki sayıları sıralı bir şekilde ekrana yazdırın.
+
+let dizi = [2, 4, 6, 8, 10, 12];
+console.log(dizi[0]);
+console.log(dizi[1]);
+console.log(dizi[2]);
+console.log(dizi[3]);
+console.log(dizi[4]);
+console.log(dizi[5]);
+
+// ! Bir metin kaç karakterden oluştuğunu yazdırın.
+
+// let metin = prompt("Bir metin giriniz:");
+// console.log(metin.length);
+
+// ! Kullanıcıdan alınan metni terse çevirerek yazdırın.
+
+let metin1 = prompt("Bir metin giriniz:");
+let ters = metin1;
+console.log(ters);
+
+
